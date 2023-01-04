f4bba79301e72fc0c9d5fb4e9e8626052a8618f2
Tyler <tyler.dins88@gmail.com>
2023-01-03 23:09:54 -0600

    html framework with css styling

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// the page loads to a welcome message and a start button 
+// the start button starts a countdown timer, has a question appear, answer choices appear
+// the user chooses selects a answer.
+// is then told, correct or wrong.
+// if question is answered wrong, then extra seconds are subtracted from timer 
+// upon selection a new question needs to appear with new answers 
+// after so many questions, the game ends
+// if the timer hits 0, the player loses
+// if the game ends before the timer hits 0, i need to record a score
+// if a score is recorded, i need a spot for user to enter name 
+// then i need a high score box to display names and scores(local storage) 
+// then i need buttons to start again 
+// need a button to clear high scores 
+// have scoreboard shown on page
+
