b2a23012b04fea09c26a9d185194b892473680d0
Tyler <tyler.dins88@gmail.com>
2023-01-05 11:06:17 -0600

    update github

--- assets/script.js
+++ assets/script.js
@@ -27,18 +27,28 @@
 
 
 var countdownEl = document.getElementById("countdown");
-var secondsLeft = 11;
+var secondsLeft = 91;
 
 var startBtn = document.querySelector(".startbtn");
 startBtn.addEventListener("click", timeDown);
 
 function timeDown() {
     // These 2 lines remove the welcome message
-    var welcome = document.getElementById("welcomePara");
-    welcome.remove();
+    var welcome = document.getElementById("answerEl");
+    if (welcome) {
+        welcome.textContent = "";
+    }
     // These 2 lines remove the start button
     var startElem = document.getElementById("startEl");
-    startElem.remove();
+    if (startElem) {
+        startElem.remove();
+    }
+    var questionElem = document.getElementById("questionEl");
+    if (questionElem) {
+        questionElem.textContent = "";
+    }
+    playBtn.remove();
+
     // This is the timer that counts down
     var countdownTimer = setInterval(function () {
         secondsLeft--;
@@ -47,60 +57,67 @@ function timeDown() {
         if (secondsLeft === 0) {
             clearInterval(countdownTimer);
             countdownEl.textContent = "Time is Out. Game Over!";
-            secondsLeft = 11;
+            secondsLeft = 91;
             // !!! need function to make high score box entry appear and play again button.
             playAgain();
-            recordScore();
-            document.getElementById("wordbox").remove();
+            document.getElementById("wordbox").textContent = "";
         }
     }, 1000)
 
     showQuestions();
 };
 
-var questionElem = document.getElementById("questionEl");
 
 var quizMaterial = [
     ["__________ is used by programmers across the world to create dynamic and interactive web content like applications and browsers.",
         ["JavaScript", "JavaMocha", "FunctionScript", "DuckRace"],
-        ["JavaScript"]],
+        [0]],
     ["The __________ method calls a function at specified intervals.",
         ["setTime", "timeInterval", "setInterval", "intervalStart"],
-        ["setInterval"]]
+        [2]],
+    ["What property allows sites to save key-value pairs in a web browser with no expiration date?",
+        ["Cloud Storage", "Local Storage", "MySpace", "Web Browser Storage"],
+        [1]]
 ]
 // will need to make a way to loop through questions when a answer is selected. 
+var question = document.createElement("h2");
 function showQuestions() {
-    questionElem.textContent = quizMaterial[0][0];
+
+    question.textContent = quizMaterial[0][0]
+    document.getElementById("wordbox").appendChild(question);
+
     for (i = 0; i < quizMaterial[0][1].length; i++) {
         if (i <= quizMaterial[0][1].length) {
             var answers = document.createElement("button");
             answers.textContent = quizMaterial[0][1][i];
             answers.className = "answerChoices";
+            answers.setAttribute("id", [i]);
             answers.setAttribute("style", "font-size: 20px")
             document.getElementById("wordbox").appendChild(answers);
         }
     }
+    var answerBtn = document.querySelectorAll(".answerChoices");
+    answerBtn.addEventListener("click", answerCheck)
 };
 
+function answerCheck() {
+    console.log("Hello");
+}
+
+var playBtn = document.createElement("button");
+playBtn.addEventListener("click", timeDown);
+
 function playAgain() {
-    var playBtn = document.createElement("button");
+    question.className = "questionPrompt"
     playBtn.textContent = "Play Again?"
     playBtn.className = "playAgain";
     document.getElementById("startAgain").appendChild(playBtn);
 }
 
+// *** need this function to store a name input & score from timer to local storage
 function recordScore() {
     var enterName = document.createElement("input");
-    enterName.textContent = "Enter Name: "
     document.getElementById("startAgain").appendChild(enterName);
 }
 
-
-
-
-// i want to make a play again button appear
-
-// i want to make the button appear where the time was
-
-// i want the button to restart the game
-
+// ADD AND REMOVE CLASSES TO HIDE THINGS
