f68c2c5ff46f9128eb6e3ee3e9cb95c669aadece
Tyler <tyler.dins88@gmail.com>
2023-01-06 21:29:38 -0600

    working on name input

--- assets/script.js
+++ assets/script.js
@@ -9,39 +9,33 @@
 
 var quizMaterial = [
     {
-        questionNum: 0,
         question: "__________ is used by programmers across the world to create dynamic and interactive web content like applications and browsers.",
         choices: ["JavaScript", "JavaMocha", "FunctionScript", "DuckRace"],
         answer: "JavaScript"
     },
     {
-        questionNum: 1,
         question: "The __________ method calls a function at specified intervals.",
-        chocies: ["setTime", "timeInterval", "setInterval", "intervalStart"],
+        choices: ["setTime", "timeInterval", "setInterval", "intervalStart"],
         answer: "setInterval"
     },
     {
-        questionNum: 2,
         question: "What property allows sites to save key-value pairs in a web browser with no expiration date?",
         choices: ["Cloud Storage", "Local Storage", "MySpace", "Web Browser Storage"],
         answer: "Local Storage"
     },
     {
-        questionNum: 3,
         question: " __________ is a social media platform used to cooperatively work on web development.",
         choices: ["GitDev", "WebDev.1", "MarkCarl", "Github"],
         answer: "Github"
     },
     {
-        questionNum: 4,
         question: " __________ is used to attach an element to another element.",
         choices: [".addElem", ".elementAdd", ".appendChild", ".appendElem"],
         answer: ".appendChild"
     },
     {
-        questionNum: 5,
         question: "The __________ property sets or returns the text content of the specified node, and all its descendants.",
-        chocies: [".textSet", ".settingText", ".textContent", ".containText"],
+        choices: [".textSet", ".settingText", ".textContent", ".containText"],
         answer: ".textContent"
     }
 ];
@@ -50,10 +44,17 @@ var startElem = document.getElementById("startEl");
 var countdownEl = document.getElementById("countdown");
 var answerElem = document.getElementById("answerEl");
 var questionElem = document.getElementById("questionEl");
-var answerBtn = document.querySelector("#answerbox");
 var playAgainBtn = document.createElement("button");
+var startAgain = document.getElementById("startAgain");
+var enterName = document.createElement("input");
+enterName.setAttribute("id", "nameBox");
+enterName.setAttribute("type", "text");
+enterName.setAttribute("class", "nameStyle")
+
 var questionNumber = 0;
 var secondsLeft = 90;
+var countdownTimer;
+
 
 var startBtn = document.querySelector(".startbtn");
 startBtn.addEventListener("click", timeDown);
@@ -68,9 +69,11 @@ function timeDown() {
     answerElem.textContent = "";
     countdownEl.textContent = "90 Seconds Left!"
     playAgainBtn.remove();
+    enterName.remove();
+    submitBtn.remove();
 
     // This is the timer that counts down
-    var countdownTimer = setInterval(function () {
+    countdownTimer = setInterval(function () {
         secondsLeft--;
         countdownEl.textContent = secondsLeft + " Seconds Left!";
         // If the timer hits 0, the game ends 
@@ -80,19 +83,18 @@ function timeDown() {
             answerbox.textContent = "";
             countdownEl.textContent = "Time is Out. Game Over!";
             secondsLeft = 90;
-            questionNumber = 0;
-            // !!! need function to make high score box entry appear and play again button.
             playAgain();
         }
     }, 1000)
-
     showQuestions(quizMaterial[questionNumber]);
 }
 
 function playAgain() {
+    secondsLeft = 90;
+    questionNumber = 0;
     playAgainBtn.textContent = "Play Again?"
     playAgainBtn.className = "playAgain";
-    document.getElementById("startAgain").appendChild(playAgainBtn);
+    startAgain.appendChild(playAgainBtn);
     playAgainBtn.addEventListener("click", timeDown);
 }
 
@@ -110,8 +112,18 @@ function showQuestions(quizTime) {
             answerChoicesEl[i].addEventListener("click", answerCheck)
             function answerCheck(event) {
                 if (event.target.textContent === quizTime.answer) {
+                    answerbox.textContent = "";
                     countdownEl.prepend("Correct! ")
-                    showQuestions(quizMaterial[questionNumber])
+                    if (questionNumber === 6) {
+                        questionElem.textContent = "Your score is " + secondsLeft + " points! Nice work!";
+                        console.log(secondsLeft)
+                        countdownEl.textContent = "You Won! Enter your name to record your score."
+                        recordScore();
+                        playAgain();
+                        clearInterval(countdownTimer);
+                    } else {
+                        showQuestions(quizMaterial[questionNumber])
+                    }
                 } else {
                     countdownEl.prepend("Wrong! ")
                     secondsLeft -= 10
@@ -122,12 +134,27 @@ function showQuestions(quizTime) {
     questionNumber++
     console.log(questionNumber)
 }
-// *** need this function to store a name input & score from timer to local storage
-// function recordScore() {
-//     var enterName = document.createElement("input");
-//     document.getElementById("startAgain").appendChild(enterName);
-// }
 
-// ADD AND REMOVE CLASSES TO HIDE THINGS
+var submitBtn = document.createElement("input");
+submitBtn.setAttribute("class", "submitBtn");
+submitBtn.setAttribute("type", "submit");
+submitBtn.setAttribute("value", "Submit");
+
+// *** need this function to store a name input & score from timer to local storage
+function recordScore() {
+    startAgain.appendChild(enterName);
+    startAgain.appendChild(submitBtn);
+    var playerName = document.getElementById("nameBox");
+    var playerScore = secondsLeft;
+    console.log(playerName);
+    console.log(playerScore);
+}
+
+submitBtn.addEventListener("submit", recordName)
+
+function recordName() {
+    console.log(enterName.val())
+}
+
 
 
